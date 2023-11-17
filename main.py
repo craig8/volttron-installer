ab27e2198d9bd90a1a82d97f745b4e72274f982a
C. Allwardt <3979063+craig8@users.noreply.github.com>
2023-11-17 12:25:10 -0800

    Update main to handle virtualenv install

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from subprocess import Popen, PIPE, check_output
+from subprocess import Popen, PIPE, check_output, check_call
 
 import os
 import time
@@ -12,159 +12,69 @@ pip_installed = False
 pexpect_installed = False
 pexpect_upgraded = False
 venv_installed = False
+INSTALLER_VENV = ".venv_installer"
+requirements = ["ansible", "nicegui", "pexpect>=4"]
 
-# Check python version. If not 3.10, prompt user to download Python 3.10 and exit program
-python_version_cmd = Popen(["bash", "-c", "python3.10 -V"], stdout=PIPE, stderr=PIPE)
-python_version = python_version_cmd.stdout.read().decode("utf-8")
-if not python_version.startswith("Python 3.10"):
-    print("Python 3.10 has not been installed. Please install Python 3.10 before continuing.")
-    print("Exiting...")
-    sys.exit(1)
 
-# Check if the required packages are installed; Install the packages if uninstalled
-print("Checking if the required packages are installed.")
-time.sleep(0.5)
+if INSTALLER_VENV not in sys.prefix:
+    # Check python version. If not 3.10, prompt user to download Python 3.10 and exit program
+    python_version_cmd = Popen(["bash", "-c", "python3.10 -V"], stdout=PIPE, stderr=PIPE)
+    python_version = python_version_cmd.stdout.read().decode("utf-8")
+    if not python_version.startswith("Python 3.10"):
+        print("Python 3.10 has not been installed. Please install Python 3.10 before continuing.")
+        print("Exiting...")
+        sys.exit(1)
 
-dpkg_output = check_output(["dpkg", "-l"])
-packages = [line.split()[1] for line in dpkg_output.decode().splitlines() if line.startswith("ii")]
+    if not sys.version_info >= (3, 10):
+        print(f"Python 3.10 is installed, but not the current version being used {sys.executable}.")
+        sys.exit(1)
+    
+    # Check if the required packages are installed; Install the packages if uninstalled
+    print("Checking if the required packages are installed.")
+    # time.sleep(0.5)
 
-pexpect_version_cmd = Popen(["bash", "-c", "pip freeze | grep pexpect"], stdout=PIPE, stderr=PIPE)
-pexpect_version = pexpect_version_cmd.stdout.read().decode("utf-8")
+    dpkg_output = check_output(["dpkg", "-l"])
+    packages = [line.split()[1] for line in dpkg_output.decode().splitlines() if line.startswith("ii")]
+    if "python3.10-venv" not in packages:
+        print("Python3.10-venv has not been installed. Now installing the python3.10-venv package using apt.")
+        Popen(["bash", "-c", "sudo apt-get install -y python3.10-venv"]).wait()
+        print("Python3.10-venv has now been installed.")
 
-nicegui_cmd = Popen(["bash", "-c", "pip freeze | grep nicegui"], stdout=PIPE, stderr=PIPE)
-nicegui_version = nicegui_cmd.stdout.read().decode("utf-8")
 
-for package in packages:
-    if package == "ansible":
-        ansible_installed = True
-    if package == "git":
-        git_installed = True
-    if package == "nicegui":
-        nicegui_installed = True
-    if package == "python3-pip":
-        pip_installed = True
-    if package == "python3.10-venv":
-        venv_installed = True
+# We know we are using the right env if it is in the prefix.
+if INSTALLER_VENV not in sys.prefix:
+    if not os.path.exists(INSTALLER_VENV):
+        result = check_call([sys.executable, "-m", "venv", INSTALLER_VENV])
+        if result != 0:
+            print("Error creating virtual environment for installer")
+            sys.exit(result)
+    env = os.environ.copy()
+    env['PATH'] = f"{os.path.abspath(INSTALLER_VENV)}/bin:{env['PATH']}"
+    result = check_call([f"{INSTALLER_VENV}/bin/python", sys.argv[0]], env=env)
+    sys.exit(result)
 
-if "nicegui" in nicegui_version:
-    nicegui_installed = True
+# Check if the required packages are installed; Install the packages if not installed
+output = check_output([sys.executable, "-m", "pip", "freeze"], text=True).splitlines()
 
-if pexpect_version.startswith("pexpect==4"):
-    pexpect_installed = True
-    pexpect_upgraded = True
-elif not pexpect_version.startswith("pexpect==4"):
-    pexpect_installed = True
-    pexpect_upgraded = False
+for rq in requirements:
+    try:
+        if ">=" in rq:
+            rq = rq.split(">=")[0]
+        elif "==" in rq:
+            rq = rq.split("==")[0]
+        found = next(filter(lambda x: x.startswith(rq), output))
+    except StopIteration:
+        print(f"Installing {rq}")
+        result = check_call([sys.executable, "-m", "pip", "install", rq])
+        if result != 0:
+            print(f"Error installing {rq}")
+            sys.exit(result)
 
-if not ansible_installed:
-    print(
-        "Ansible has not been installed. Now installing the ansible package using apt."
-    )
-    Popen(["bash", "-c", "sudo apt-get install -y ansible"]).wait()
-    print("Ansible has now been installed.")
-elif ansible_installed:
-    print("Ansible is already installed.")
+if not os.path.exists(os.path.expanduser("~/.ansible/collections/ansible_collections/volttron")):
+    result = check_call(["ansible-galaxy", "collection", "install", "git+https://github.com/VOLTTRON/volttron-ansible.git,develop"])
 
-if not git_installed:
-    print("Git has not been installed. Now installing the git package using apt.")
-    Popen(["bash", "-c", "sudo apt-get install -y git"]).wait()
-    print("Git has now been installed.")
-elif git_installed:
-    print("Git is already installed.")
-
-if not nicegui_installed:
-    print("NiceGUI has not been installed. Now installing the NiceGUI package with pip")
-    Popen(["bash", "-c", "pip install nicegui"]).wait()
-    print("NiceGUI has been installed")
-elif nicegui_installed:
-    print("NiceGUI is already installed")
-
-if not pip_installed:
-    print("Pip has not been installed. Now installing the pip package using apt.")
-    Popen(["bash", "-c", "sudo apt install python3-pip"]).wait()
-    print("Pip has now been installed.")
-elif pip_installed:
-    print("Pip is already installed.")
-
-if not pexpect_installed:
-    print("Pexpect has not been installed. Now installing the pexpect package.")
-    Popen(["bash", "-c", "pip install pexpect"]).wait()
-    print("Pexpect has been installed to its latest version.")
-elif pexpect_installed and not pexpect_upgraded:
-    print(
-        "Pexpect is installed but is not the correct version. Now updating the pexpect package."
-    )
-    Popen(["bash", "-c", "pip install --upgrade pexpect"]).wait()
-    print("Pexpect has been upgraded to its latest version.")
-elif pexpect_installed and pexpect_upgraded:
-    print("Pexpect has already been installed")
-
-if not venv_installed:
-    print("Python3.10-venv has not been installed. Now installing the python3.10-venv package using apt.")
-    Popen(["bash", "-c", "sudo apt-get install -y python3.10-venv"]).wait()
-    print("Python3.10-venv has now been installed.")
-elif venv_installed:
-    print("Python3.10-venv is already installed.")
-
-"""
-    Checks whether a virtual environment under .venv exists in the current directory; 
-    If not, create a virtual environment under the current directory as .venv;
-    If there is a virtual environment under .venv, ask the user if they want to 
-    override the current .venv or name the virtual environment as something else.
-"""
-print("Now checking if the virtual environment '.venv' exists.")
-time.sleep(1)
-if not os.path.exists(".venv"):
-    print("The virtual environment '.venv' does not exist. Now creating the virtual environment as .venv")
-    Popen(["bash", "-c", "python3.10 -m venv .venv"]).wait()
-    print("The virtual environment '.venv' has been created")
-    override = True
-elif os.path.exists(".venv"):
-    while True:
-        override = str(input("The virtual environment '.venv' does exist. Would you like to override it? (yes or no) "))
-        if override.upper().startswith("Y"):
-            override = True
-            venv_name = False
-            print("Now overriding the current virtual environment.")
-            Popen(["bash", "-c", "python3.10 -m venv .venv"]).wait()
-            print("The virtual environment '.venv' has been created and has overridden the previous virtual environment.")
-            break
-        elif override.upper().startswith("N"):
-            override = False
-            venv_name = str(input("What would you like the new virtual environment to be named? "))
-            print(f"Now creating the virtual environment '{venv_name}'.")
-            Popen(["bash", "-c", f"python3.10 -m venv {venv_name}"]).wait()
-            print(f"The virtual environment '{venv_name}' has been created.")
-            break
-        else:
-            print("Please enter yes or no.")
-
-# Activate the virtual environment and install volttron-ansible depending on whether the venv was overriden or not.
-if override:
-    print("Now checking if the collection 'volttron-ansible' is installed.")
-    time.sleep(1)
-    if not os.path.exists(os.path.expanduser("~") + "/.ansible/collections/ansible_collections/volttron"):
-        print("The collection 'volttron-ansible' is not installed.")
-        print("Now activating the virtual environment '.venv' and installing the collection 'volttron-ansible'.")
-        Popen(["bash", "-c", "source .venv/bin/activate && ansible-galaxy collection install git+https://github.com/VOLTTRON/volttron-ansible.git,develop"]).wait()
-        print("The collection 'volttron-ansible' has been installed inside the virtual environment '.venv'.")
-    else:
-        print("The collection 'volttron-ansible' is already installed.")
-
-if not override:
-    print("Now checking if the collection 'volttron-ansible' is installed.")
-    time.sleep(1)
-    if not os.path.exists(os.path.expanduser("~/.ansible/collections/ansible_collections/volttron")):
-        print("The collection 'volttron-ansible' has not been installed.")
-        print(f"Now activating the virtual environemnt '{venv_name}' and installing the collection 'volttron-ansible'.")
-        Popen(["bash", "-c", f"source {venv_name}/bin/activate && ansible-galaxy collection install git+https://github.com/VOLTTRON/volttron-ansible.git,develop"]).wait()
-        print(f"The collection 'volttron-ansible' has been installed inside the virtual environment '{venv_name}'.")
-    else:
-        print("The collection 'volttron-ansible' is already installed.")
-
-# Clone repo so web server can access files related to the web server
-print("Cloning volttron-installer repository so the web server can access required files")
-Popen(["bash", "-c", "git clone --branch niceGUI https://github.com/VOLTTRON/volttron-installer.git"]).wait()
+if not os.path.exists("volttron-installer"):
+    Popen(["bash", "-c", "git clone --branch virtualenv https://github.com/craig8/volttron-installer.git", "volttron-installer"]).wait()
 
 # Start NiceGUI
-Popen(['bash', '-c', 'python3 volttron-installer/pages.py']).wait()
+Popen([sys.executable, "volttron-installer/pages.py"]).wait()
