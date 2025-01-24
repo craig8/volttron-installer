cb147fc680672c6c695f942b26c4e487fbd947e5
dominrios <dominic.rios@pnnl.gov>
2025-01-24 12:25:24 -0800

    added a param for form_entry styling, "required_entry"

--- volttron_installer/frontend/frontend/components/form_components/form_entry.py
+++ volttron_installer/frontend/frontend/components/form_components/form_entry.py
@@ -1,13 +1,20 @@
 import reflex as rx
 
 
-def form_entry( text: str, input: rx.Component, upload: rx.Component | None = None) -> rx.Component:
+def form_entry( text: str, input: rx.Component, upload: rx.Component | None = None, required_entry: bool = False) -> rx.Component:
     if upload is None:
         upload = rx.el.div()
 
     return rx.flex(
         rx.flex(
-            rx.text(text),
+            rx.text(
+                text,
+                rx.cond(
+                        required_entry,
+                        rx.text.span(" *", color="red"),
+                        rx.text.span("")
+                    )
+                ),
             upload,
             align="start",
             justify="between",
